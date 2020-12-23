eb5fe18f8eeb0a5829aa0020c9ba9bd3bea3ba5c
Dusan Juranovic <dusan.juranovic@gmail.com>
2020-12-23 17:16:14 +0100

    Add files via upload
    Added CocoaPods support

---
+++ Example/HyperlinkUILabel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HyperlinkUILabel
+//
+//  Created by Yoorque on 12/23/2020.
+//  Copyright (c) 2020 Yoorque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/HyperlinkUILabel/HyperlinkUILabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HyperlinkUILabel : NSObject
+@end
+@implementation PodsDummy_HyperlinkUILabel
+@end
